10d2b5d5c80c6d4075beadc0412166afb779ca5f
andreas4211631018 <andreas.supriyanta@gmail.com>
2016-10-20 08:33:01 +0700

    Add files via upload

---
+++ RS485_Slave1.ino
@@ -0,0 +1,18 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial RS485Serial(10,11);
+
+
+void setup()
+{
+  pinMode(3,OUTPUT);
+  digitalWrite(3,LOW);
+  RS485Serial.begin(9600);
+}
+void loop()
+int data = analogRead(A0);
+{
+  digitalWrite(3,HIGH);
+  RS485Serial.write(data);
+  delay(1000);
+}

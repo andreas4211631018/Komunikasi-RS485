b312b45d91ccb3ce1eaddfb91d79692ecd8ebaca
andreas4211631018 <andreas.supriyanta@gmail.com>
2016-10-20 08:33:31 +0700

    Add files via upload

---
+++ RS485_Master2.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h>
+SoftwareSerial RS485Serial(10,11); 
+int data;
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Komunikasi RS485 : ");
+  pinMode(3,OUTPUT);
+  digitalWrite(3,LOW);
+  RS485Serial.begin(9600);
+}
+void loop()
+{
+  if (RS485Serial.available())
+  {
+    data = RS485Serial.read();
+    Serial.println();
+  }
+}
+
